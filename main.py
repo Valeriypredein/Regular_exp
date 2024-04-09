cf8ff4ed390ee2bec2f7c7d7bb98603341de1d7a
Valeriy <112763744+Valeriypredein@users.noreply.github.com>
2024-04-09 20:42:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ import re
 with open("phonebook_raw.csv", encoding="utf-8") as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
-# pprint(contacts_list)
-
 
 # Задание № 1: поместить Фамилию, Имя и Отчество человека в поля
 # lastname, firstname и surname соответственно.
@@ -19,7 +17,6 @@ for item in contacts_list:
     sub_list.extend(j.split())
   sub_list.extend(item[3:])
   full_name.append(sub_list)
-# pprint(full_name)
 
 # Задание № 2: привести все телефоны в формат +7(999)999-99-99.
 # Если есть добавочный номер, формат будет такой: +7(999)999-99-99 доб.9999.
@@ -28,7 +25,6 @@ for item in full_name:
     pattern_phone = r"(\+7|8)(\s?\(?)(\d{3})(\-?\)?\s?)(\d{3})(\-?)(\d{2})(\-?)(\d{2})(\s?)(\(?)([д]?[о]?[б]?\.?)(\s?)(\d?\d?\d?\d?)(\)?)"
     result_phone = re.sub(pattern_phone, r"+7(\3)\5-\7-\9\10\12\14", item[5])
     item[5] = result_phone
-pprint(full_name)
 
 # Задание № 3: Объединить все дублирующиеся записи о человеке в одной.
 # # Подсказка: группируйте записи по ФИО (если будет сложно, группировать только по ФИ).
